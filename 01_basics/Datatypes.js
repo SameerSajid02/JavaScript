77a1f73f291f6b4fdbdb0de2ef5d106872ca32c4
SameerSajid02 <138728570+SameerSajid02@users.noreply.github.com>
2025-02-26 16:46:30 +0000

    container config done

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+
+
+
+let name = "Sameer"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//Primitive Datatype
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//Non-Primitive Datatype
+// object
+//Arrays
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object ,, Although its a weakness but it is what it is....
